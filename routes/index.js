253f2b152ebff4f9c5cd09fa6040609b8cde6632
Stephan <ssomers42@gmail.com>
2018-02-21 13:22:02 -0800

    IndexLogged works with Json

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
+var data = require('../data.json');
 
 /*
  * GET home page.
  */
-
 exports.view = function(req, res){
+	console.log('hello');
   res.render('index');
 };
 
 exports.logged = function(req, res) {
-	res.render('indexLogged');
+	res.render('indexLogged', data);
 };
